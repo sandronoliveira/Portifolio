27307e4ee3fa3971e34713882c5be27a90f3b749
Sandron Oliveira Silva <sandrono61@gmail.com>
2025-03-07 01:03:20 -0300

    add a Procfile

--- app.py
+++ app.py
@@ -523,4 +523,4 @@ def obrigado():
     return render_template('obrigado.html', user=User.query.first())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
